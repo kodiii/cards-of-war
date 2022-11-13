530ca942daa3b5b77ea4b8f0b3cea2757ba376f0
Ricardo Ribeiro <kodii@kodii.eu>
2022-11-13 19:43:36 +0100

    check winner card function

--- index.js
+++ index.js
@@ -18,13 +18,35 @@ document.getElementById("draw-cards").addEventListener("click", () => {
         .then(res => res.json())
         .then(data => {
             console.log(data.cards)
+
             let html = ""
 
             data.cards.forEach(card => {
+
+
                 html += `
                        <img src=${card.image} />                    
                 `
-            document.querySelector(".placeholder").innerHTML = html
+            document.getElementById("placeholder").innerHTML = html
             })
+
+            determineCardWinner(data.cards[0], data.cards[1])
         })
 })
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "JACK", "QUEEN", "KING", "ACE"]
+
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+
+    if (card1ValueIndex > card2ValueIndex) {
+        message.innerText = "COMPUTER WIN!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log("PLAYER WIN!")
+        message.innerText = "PLAYER WIN!"
+    } else {
+        message.innerText = "TYE"
+    }
+}
