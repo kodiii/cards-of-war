5167ebab3d003d034f0864154b998a216d690b87
Ricardo Ribeiro <kodii@kodii.eu>
2022-11-16 09:06:04 +0100

    code refactoring

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 let deckId
 const message = document.getElementById("message")
+const newDexk = document.getElementById("new-deck")
+const drawCards = document.getElementById("draw-cards")
 const cardsRemaining = document.getElementById("cards-remaining")
 const computer = document.getElementById("computer")
 const player = document.getElementById("player")
 let computerCount = 0
 let playerCount = 0
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDexk.addEventListener("click", handleClick)
 
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            deckId = data.deck_id
-            resetGame(data.remaining)
-        })
+async function handleClick() {
+    const res = await fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    const data = await res.json()
+    deckId = data.deck_id
+    resetGame(data.remaining)
+
 }
 
-document.getElementById("draw-cards").addEventListener("click", () => {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-        .then(res => res.json())
-        .then(data => {
-            let html = ""
+drawCards.addEventListener("click", async () => {
+    const res = await fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    const data = await res.json()
+    let html = ""
 
-            data.cards.forEach(card => {
-                html += `
-                       <img src=${card.image} />                    
-                `
-            document.getElementById("placeholder").innerHTML = html
-            })
+    data.cards.forEach(card => {
+        html += `
+               <img src=${card.image} />                    
+        `
+    document.getElementById("placeholder").innerHTML = html
+    })
 
-            determineCardWinner(data.cards[0].value, data.cards[1].value)
-            cardsRemaining.style.display = "block"
-            cardsRemaining.innerText = `Cards remaining: ${data.remaining}`
-            gameEndMessage(data.remaining)
-        })
+    determineCardWinner(data.cards[0].value, data.cards[1].value)
+    cardsRemaining.style.display = "block"
+    cardsRemaining.innerText = `Cards remaining: ${data.remaining}`
+    gameEndMessage(data.remaining)
 })
 
 function determineCardWinner(card1, card2) {
@@ -62,9 +60,11 @@ function determineCardWinner(card1, card2) {
 
 function gameEndMessage(cardsRemain) {
     if(cardsRemain === 0 && computerCount > playerCount) {
+        drawCards.disabled = true
         message.style.color = "red"
         message.textContent = " Game End, 'Computer' WIN the WAR"
     } else if (cardsRemain === 0 && computerCount < playerCount) {
+        drawCards.disabled = true
         message.style.color = "red"
         message.textContent = " Game End, 'Player' WIN the WAR"
     }
